cd92e5e3680a1f93e19665b2c6b9c8f993e21be8
hvortex <hebertms2009@hotmail.com>
2024-10-27 16:13:02 -0300

    final

--- acai/src/TesteDecorator.java
+++ acai/src/TesteDecorator.java
@@ -1,14 +1,12 @@
 public class TesteDecorator {
     public static void main(String[] args) {
-        // Açaí Simples - Pequeno
+
         Adicional acaiSimples = new Acai(Tamanho.PEQUENO);
         System.out.println(acaiSimples.getDescricao() + " custa R$ " + acaiSimples.custo());
 
-        // Açaí com Morango - Médio
         Adicional acaiMorango = new Morango(new Acai(Tamanho.MEDIO), Tamanho.MEDIO);
         System.out.println(acaiMorango.getDescricao() + " custa R$ " + acaiMorango.custo());
 
-        // Açaí com Morango, Leite Condensado e Banana - Grande
         Adicional acaiCompleto = new Banana(new LeiteCondensado(new Morango(new Acai(Tamanho.GRANDE), Tamanho.GRANDE), Tamanho.GRANDE), Tamanho.GRANDE);
         System.out.println(acaiCompleto.getDescricao() + " custa R$ " + acaiCompleto.custo());
     }
